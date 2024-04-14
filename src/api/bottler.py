b5d8f8ec0dd5ee31c80df5a5460755b0085a56da
Martin Wise <mmwise2013@gmail.com>
2024-04-14 14:25:34 -0700

    updated bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -82,7 +82,7 @@ def transfer_to_global_inventory(potion: PotionInventory):
         current_num_green_ml = get_green_ml()
         current_num_green_potions = get_num_green_potions()
 
-        new_num_green_potions = current_num_green_potions - potion.quantity
-        new_num_green_ml = current_num_green_ml - (100 * potion.quantity )
+        new_num_green_potions = current_num_green_potions + potion.quantity
+        new_num_green_ml = current_num_green_ml - (100 * potion.quantity)
 
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_potions = {new_num_green_potions}, num_green_ml = {new_num_green_ml}"))
