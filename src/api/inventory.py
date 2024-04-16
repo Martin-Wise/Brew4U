0aab998d9fc28d9f953d106cc099f1e91663b337
Martin Wise <mmwise2013@gmail.com>
2024-04-16 12:51:27 -0700

    updated inventory.py, added audit functionality

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/inventory",
@@ -12,8 +14,26 @@ router = APIRouter(
 @router.get("/audit")
 def get_inventory():
     """ """
-    
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    number_of_potions = 0
+    ml_in_barrels = 0
+    gold = 0
+
+    with db.engine.begin() as connection:
+        cur_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
+        gold = cur_gold.fetchone()[0]
+        
+        red_ml = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory"))
+        ml_in_barrels += red_ml.fetchone()[0]
+        green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
+        ml_in_barrels += green_ml.fetchone()[0]
+        blue_ml = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory"))
+        ml_in_barrels += blue_ml.fetchone()[0]
+
+        all_potions = connection.execute(sqlalchemy.text("SELECT num FROM potion_inventory"))
+        for potion in all_potions:
+            number_of_potions += potion[0]
+
+    return {"number_of_potions": number_of_potions, "ml_in_barrels": ml_in_barrels, "gold": gold}
 
 # Gets called once a day
 @router.post("/plan")
